900d7b8fba3e72b7e31b18504189137c105d44e4
yuanfang <65866693@qq.com>
2017-06-21 12:20:01 +0800

    file

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ var app=express();
 
 var fs=require('fs');
 
+var users;
+
+function readUserFile(){
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        users=data;
+        console.log( data );
+    });
+    // fs.close( __dirname + "/" + "users.json");
+}
+
+readUserFile();
+
 app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
+    res.end(users);
+//    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+//        console.log( data );
+//        users=data;
+//        res.end( data );
+//    });
 });
 
 //添加的新用户数据
@@ -22,34 +36,28 @@ var user = {
 
 //add
 app.get('/addUser', function (req, res) {
-   // 读取已存在的数据
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       jdata = JSON.parse( data );
+       jdata = JSON.parse( users );
        jdata["user4"] = user["user4"];
        console.log( jdata );
        res.end( JSON.stringify(jdata));
    });
-})
 
 //get
 app.get('/user/:id', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       jdata = JSON.parse( data );
+       jdata = JSON.parse( users );
        var user = jdata["user" + req.params.id] 
        console.log( user );
        res.end( JSON.stringify(user));
    });
-})
 
 //delete
 app.get('/deleteUser/:id',function(req,res){
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       jdata = JSON.parse( data );
+       jdata = JSON.parse( users );
        delete jdata["user"+req.params.id]
        console.log( jdata );
        res.end( JSON.stringify(jdata));
    });
-})
+
 
 var server = app.listen(8081, function () {
 
